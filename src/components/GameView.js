87045eaab8867331f519169f513ed3fe3344c2a5
Dan Chung <danetix@gmail.com>
2018-09-07 10:14:30 -0400

    removed socketio, engineio

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,5 +1,9 @@
 import React,  { Component } from 'react'
 import GameCharades from './GameCharades'
+// import io from 'socket.io'
+
+// const io = require('socket.io')()
+// const io = require('socket.io')(80, { wsEngine: 'ws' });
 
 export default class GameView extends Component {
   render() {
