1c2e62f4e4bd1cf61de05174e51bf2913e6d65cf
github username <github email address>
2018-09-07 11:22:34 -0400

    Front end ain't listening

--- src/components/Cables.js
+++ src/components/Cables.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import {ActionCable} from 'react-actioncable-provider'
+import { ActionCable } from 'react-actioncable-provider'
 
-export default class Cable extends React.Component{
+export default class Cables extends React.Component{
+
+  state = {
+    response: ''
+  }
+
+
+
+  onReceived = (e) => {
+    console.log(e)
+
+  }
+
+  sendMessage = () => {
+    console.log(this.refs)
+
+    const message = this.refs.newMessage.value
+    this.refs.ChatChannel.perform('onChat', {message})
+
+  }
+
+  // componentDidMount(){
+  //
+  // }
 
   render(){
+    console.log(this.subscription)
     return(
+      <div>
       <ActionCable
-        channel={{channel: 'game_line_channel'}}
-        onReceived={console.log}
+        ref='ChatChannel'
+        channel={{channel: 'ChatChannel'}}
+        onReceived={(e) => console.log(e)}
+        onReceived={this.onReceived}
        />
+       <p>{this.state.response}</p>
+        <input ref='newMessage' type='text' />
+        <button onClick={this.sendMessage}>Send</button>
+        </div>
     );
   }
 }
