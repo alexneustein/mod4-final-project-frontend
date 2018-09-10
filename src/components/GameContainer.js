7536c83855ac90449332b5d87ebaafd7624c39ff
Dan Chung <danetix@gmail.com>
2018-09-10 17:55:14 -0400

    it works! cleaned up code

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -121,7 +121,7 @@ export default class GameContainer extends Component {
           channel={{channel:'ScoreChannel'}}
           onReceived={this.scoreReceived}
         />
-        {/* <GameView /> */}
+        <GameView />
         <h2>{this.state.answer}</h2>
         {this.state.gameOn
           ? <MessageInput inputChange={this.inputChange} controlField={this.state.guessField} score={this.state.score} setScore={this.setScore}/>
