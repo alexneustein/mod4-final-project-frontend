9ec104af963b3f7d03a121cb5c389107d105b796
Dan Chung <danetix@gmail.com>
2018-09-12 23:31:54 -0400

    game audio

--- src/components/GameAudiooInitiator.js
+++ src/components/GameAudiooInitiator.js
@@ -1,73 +1,120 @@
 import React,  { Component } from 'react'
 import { ActionCable } from 'react-actioncable-provider'
+require('events').EventEmitter.prototype._maxListeners = 100
 const Peer = require('simple-peer')
 
 let p1
-let streamObj
+// let reconnectSignal
 
 export default class GameAudiooInitiator extends Component {
   state = {
-    signal: ""
+    signal: {}
   }
 
   componentDidMount() {
-    navigator.mediaDevices.getUserMedia({ video: false, audio: true })
-      .then(stream => {
-        p1 = new Peer({ initiator: true, stream: stream, trickle: false })
-        // console.log("STREAM OBJ", stream)
-        streamObj = stream
-
-        const video = document.querySelector('video')
-        video.srcObject = stream
-        video.play()
-
-        p1.on('signal', (data) => {
-          // console.log('p1 signal', data)
-          this.sendMessage('send_signal', {init_signal: data})
-        })
-      })
+    window.setTimeout(this.initPeer, 2000)
+    // reconnectSignal = window.setInterval(() => {
+    //   this.initPeer()
+    // }, 3000)
   }
 
   componentDidUpdate() {
     // console.log("componentDidUpdate SIGNAL", this.state.signal)
 
-    if('rec_signal' in this.state.signal) {
-      p1.signal(this.state.signal.rec_signal)
-    }
+    if(p1) {
+      if('rec_signal' in this.state.signal) {
+        // window.clearInterval(reconnectSignal)
+        // console.log("RECSIGNAL", p1)
+        const signal = {...this.state.signal.rec_signal}
 
-    if('rec_stream' in this.state.signal) {
-      this.sendMessage('send_signal', {init_stream: streamObj})
-    }
+        this.setState({signal: {}}, () => {
+          p1.signal(signal)
+          // console.log("SIGNAFDAFA", this.state)
+        })
+      }
 
-    p1.on('connect', () => {
-      // console.log('p1 connected')
-    })
-    p1.on('error', (error) => console.error('p1 error', error))
-    p1.on('close', () => console.log('p1 connection closed'))
+      if('rec_stream' in this.state.signal) {
+        this.setState({signal: {}}, () => {
+          this.sendSignal('send_signal', {init_stream: true})
+        })
+      }
+
+      p1.on('stream', (stream) => {
+        // console.log('p1 received', stream)
+        const video = document.querySelector('video#received-video')
+        video.srcObject = stream
+        // video.play()
+      })
+
+      p1.on('connect', () => {
+        // console.log('p1 connected')
+      })
+      p1.on('error', (error) => console.error('p1 error', error))
+      p1.on('close', () => {
+        console.log('p1 connection closed')
+        p1.removeListener('signal', p1.signal)
+        // console.log("CLOSE", p1)
+        // this.initPeer()
+      })
+    }
   }
 
   onReceived = e => {
     if(e.data.forInitiator) {
       this.setState({signal: e.data.forInitiator}, () => {
-        // console.log("forInitiator", this.state.signal)
+        console.log("forInitiator", this.state.signal)
       })
+    }
+    if(e.data.reconnect) {
+      p1.destroy()
+      this.initPeer()
+      console.log("RECONNECTING")
     }
   }
 
-  sendMessage = (method, forReceiver) => {
+  sendSignal = (method, forReceiver) => {
     this.refs.gameSignalChannel.perform(method, {forReceiver})
   }
 
-  testMessage = () => {
+  reconnectSignal = (method, reconnect) => {
+    this.refs.gameSignalChannel.perform(method, {reconnect})
+  }
+
+  reconnect = () => {
+    this.reconnectSignal('send_signal', {reconnect: true})
+    console.log("test")
+  }
+
+  initPeer = () => {
+    navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+      .then(stream => {
+        p1 = new Peer({ initiator: true, stream: stream, trickle: false })
+        // console.log("STREAM OBJ", stream)
+        // console.log("INITPEER", p1)
+
+        const video = document.querySelector('video#local-video')
+        video.srcObject = stream
+        // video.play()
+
+
+      })
+      .then(() => {
+        p1.on('signal', (data) => {
+          // console.log('p1 signal', data)
+          this.sendSignal('send_signal', {init_signal: data})
+        })
+      })
   }
 
   render() {
     return (
       <div>
         <ActionCable ref="gameSignalChannel" channel={{channel: 'GameSignalChannel'}} onReceived={this.onReceived} />
-        {/* <ActionCable ref="gameChannelReceiver" channel={{channel: 'GameChannelReceiver'}} /> */}
-        {/* <video id="received_video" autoPlay muted></video> */}
-        <video id="local_video" autoPlay muted></video>
+        <div id="video-wrapper">
+          <video id="local-video" autoPlay muted></video>
+          <video id="received-video" autoPlay muted></video>
+          <button onClick={this.reconnect}>Reconnect</button>
+        </div>
       </div>
     )
   }
--- src/components/GameAudiooReceiver.js
+++ src/components/GameAudiooReceiver.js
@@ -1,63 +1,102 @@
 import React,  { Component } from 'react'
 import { ActionCable } from 'react-actioncable-provider'
+require('events').EventEmitter.prototype._maxListeners = 100
 const Peer = require('simple-peer')
 
 let p1
 
 export default class GameAudiooReceiver extends Component {
   state = {
-    signal: ""
+    signal: {}
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.initPeer()
+  }
 
   componentDidUpdate() {
     // console.log("componentDidUpdate SIGNAL", this.state.signal)
 
-    if('init_signal' in this.state.signal) {
-      p1 = new Peer({ trickle: false })
-      p1.signal(this.state.signal.init_signal)
-      p1.on('signal', (data) => {
-        // console.log('p1 signal', data)
-        this.sendMessage('send_signal', {rec_signal: data})
+    if(p1) {
+      if('init_signal' in this.state.signal) {
+        const signal = {...this.state.signal.init_signal}
+
+        this.setState({signal: {}}, () => {
+          p1.signal(signal)
+          p1.once('signal', (data) => {
+            console.log('p1 signal', data)
+            this.sendSignal('send_signal', {rec_signal: data})
+          })
+        })
+      }
+
+      p1.on('stream', (stream) => {
+        // console.log('p1 received', stream)
+        const video = document.querySelector('video#received-video')
+        video.srcObject = stream
+        // video.play()
+      })
+
+      p1.on('connect', () => {
+        // console.log('p1 connected')
+        this.sendSignal('send_signal', {rec_stream: true})
+      })
+      p1.on('error', (error) => console.error('p1 error', error))
+      p1.on('close', () => {
+        console.log('p1 connection closed')
+        p1.removeListener('signal', p1.signal)
+        // this.initPeer()
       })
     }
-
-    p1.on('stream', (stream) => {
-      // console.log('p1 received', stream)
-      const video = document.querySelector('video')
-      video.srcObject = stream
-      video.play()
-    })
-
-    p1.on('connect', () => {
-      // console.log('p1 connected')
-      this.sendMessage('send_signal', {rec_stream: ""})
-    })
-    p1.on('error', (error) => console.error('p1 error', error))
-    p1.on('close', () => console.log('p1 connection closed'))
   }
 
 
   onReceived = e => {
     if(e.data.forReceiver) {
       this.setState({signal: e.data.forReceiver}, () => {
-        // console.log("forReceiver", this.state.signal)
+        console.log("forReceiver", this.state.signal)
       })
+    }
+    if(e.data.reconnect) {
+      p1.destroy()
+      this.initPeer()
+      console.log("RECONNECTING")
     }
   }
 
-  sendMessage = (method, forInitiator) => {
+  sendSignal = (method, forInitiator) => {
     this.refs.gameSignalChannel.perform(method, {forInitiator})
+  }
+
+  reconnectSignal = (method, reconnect) => {
+    this.refs.gameSignalChannel.perform(method, {reconnect})
+  }
+
+  reconnect = () => {
+    this.reconnectSignal('send_signal', {reconnect: true})
+    console.log("test")
+  }
+
+  initPeer = () => {
+    navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+      .then(stream => {
+        p1 = new Peer({ trickle: false, stream: stream })
+
+        const video = document.querySelector('video#local-video')
+        video.srcObject = stream
+        // video.play()
+      })
   }
 
   render() {
     return (
       <div>
         <ActionCable ref="gameSignalChannel" channel={{channel: 'GameSignalChannel'}} onReceived={this.onReceived} />
-        {/* <ActionCable ref="gameChannelReceiver" channel={{channel: 'GameChannelReceiver'}} onReceived={this.onReceived} /> */}
-        <video id="received_video" autoPlay muted></video>
-        {/* <video id="local_video" autoPlay muted></video> */}
+        <div id="video-wrapper">
+          <video id="local-video" autoPlay muted></video>
+          <video id="received-video" autoPlay muted></video>
+          <button onClick={this.reconnect}>Reconnect</button>
+        </div>
       </div>
     )
   }
